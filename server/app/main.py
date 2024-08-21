e2659cb3cb1c7fe21256c0b53f5f2d9e8000a0b5
Contactsushil <contact.sushilpandey@gmail.com>
2024-08-21 18:49:05 +0530

    Removed React

--- server/app/main.py
+++ server/app/main.py
@@ -1,9 +1,40 @@
 # main.py
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from app.models.user import User as User_model
 from app.db.base import engine
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import os
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory=os.path.abspath("/Users/sushilpandey/Documents/Mine/Shiver/server/app/templates"))
+app.mount("/static", StaticFiles(directory=os.path.abspath("/Users/sushilpandey/Documents/Mine/Shiver/server/app/static")), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="home.html"
+    )
+
+@app.get("/dashboard", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="Dashboard.html"
+    )
+
+@app.get("/chat", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="Chat.html"
+    )
+
+@app.get("/recipie", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="Recipie.html"
+    )
 
 # Ensure all tables are created
 User_model.metadata.create_all(bind=engine)
